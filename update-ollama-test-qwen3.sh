ffa86216b678cbc259a62ff387522689cb23664b
shiyuanyou <shiyuanyou@bigo.sg>
2025-05-06 15:37:50 +0800

    chore: 恢复注释逻辑

--- update-ollama-test-qwen3.sh
+++ update-ollama-test-qwen3.sh
@@ -3,11 +3,11 @@
 model_name="qwen3:30b-a3b"
 
 # both update and install
-# curl -fsSL https://ollama.com/install.sh | sh
+curl -fsSL https://ollama.com/install.sh | sh
 echo "Ollama 已安装"
 
 # auto listen 0.0.0.0:11434 instead of 127.0.0.1:11434
-# ./expose-ollama.sh
+./expose-ollama.sh
 echo "Ollama 服务已自动开放监听外部访问"
 
 # 检查是否有 -t 参数
